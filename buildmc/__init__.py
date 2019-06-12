cf12f42a72c53b21a9210f89af9d0e2bb485f823
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2019-06-12 12:36:17 -0400

    give better feedback when source build file not found

--- buildmc/__init__.py
+++ buildmc/__init__.py
@@ -19,7 +19,7 @@ def do_build(params: Dict[str, Union[str, Path]],
 
     params['source_dir'], params['build_dir'] = get_dirs(params['source_dir'], params['build_dir'])
 
-    build_system = _get_buildsystem(params['build_system'], params['source_dir'])
+    build_system = get_buildsystem(params['build_system'], params['source_dir'])
 
     if build_system == 'meson':
         meson.meson_config(params, compilers, args, wipe=wipe, dotest=dotest)
@@ -34,14 +34,9 @@ def do_build(params: Dict[str, Union[str, Path]],
 def get_dirs(source_dir: Union[str, Path],
              build_dir: Union[str, Path]) -> Tuple[Path, Path]:
 
-    if source_dir:
-        source_dir = Path(source_dir).expanduser().resolve()
-        if not source_dir.is_dir():
-            raise NotADirectoryError(source_dir)
-    else:
-        source_dir = Path().cwd()
-        if not source_dir.is_dir():
-            raise SystemExit('Please specify a source directory.   buildmc -s mydirectory')
+    source_dir = Path(source_dir).expanduser().resolve() if source_dir else Path().cwd()
+
+    find_buildfile(source_dir)
 
     if build_dir:
         build_dir = Path(build_dir).expanduser().resolve()
@@ -55,27 +50,31 @@ def get_dirs(source_dir: Union[str, Path],
     return source_dir, build_dir
 
 
-def _get_buildsystem(build_system: Union[str, Path], source_dir: Union[str, Path]) -> str:
-    """
-    if user didn't pick a build system, try to figure out which is appropriate for the project
-    """
-
-    if not build_system:
-        pass
-    elif isinstance(build_system, str):
-        return build_system
-    elif isinstance(build_system, Path):
-        return build_system.stem
-    else:
-        raise TypeError(build_system)
+def find_buildfile(source_dir: Union[str, Path]) -> str:
 
     source_dir = Path(source_dir).expanduser()
+
+    if not source_dir.is_dir():
+        raise NotADirectoryError(source_dir)
 
     if (source_dir / 'CMakeLists.txt').is_file():
         return 'cmake'
     elif (source_dir / 'meson.build').is_file():
         return 'meson'
-    elif (source_dir / 'Makefile').is_file():
-        return 'gnumake'
 
-    raise ValueError('Unable to determmine build system automatically')
+    raise FileNotFoundError(f'could not find build system file (CMakeLists.txt or meson.build) in {source_dir}')
+
+
+def get_buildsystem(build_system: Union[str, Path], source_dir: Union[str, Path]) -> str:
+    """
+    if user didn't pick a build system, try to figure out which is appropriate for the project
+    """
+
+    if not build_system:
+        return find_buildfile(source_dir)
+    elif isinstance(build_system, str):
+        return build_system
+    elif isinstance(build_system, Path):
+        return build_system.stem
+
+    raise TypeError(build_system)
